0dcb9902865d02e59cf847985d10a1e00395fb8c
Derick Zhang <zhangd2@mascorp.com>
2018-06-04 21:38:23 -0700

    Added function to merge two lists

--- 148. Sort List.java
+++ 148. Sort List.java
@@ -15,3 +15,16 @@ public ListNode sortList(ListNode head) {
         
 }
 
+public ListNode mergeTwoLists(ListNode a, ListNode b){
+	if(a == null) return b;
+	if(b == null) return a;
+	if(a.val < b.val){
+		a.next = mergeTwoLists(a.next, b);
+		return a;
+	}
+	else{
+		b.next = mergeTwoLists(a, b.next);
+		return b;
+	}
+}
+
