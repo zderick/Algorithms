09ee64ab6a67a9088e21e4c5a011ad9244db837c
Derick Zhang <zhangd2@mascorp.com>
2018-06-13 17:25:58 -0700

    Initial commit

---
+++ 739. Daily Temperatures.java
@@ -0,0 +1,18 @@
+/*
+Given a list of daily temperatures, produce a list that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+
+For example, given the list temperatures = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+*/
+public int[] dailyTemperatures(int[] temperatures) {
+	int[] result = new int[temperatures.length];
+	Deque<Integer> stack = new LinkedList<>();
+	for(int i = 0; i < temperatures.length; i++){
+		while(!stack.isEmpty() && nums[stack.peekFirst()] < nums[i]){
+			result[stack.peekFirst()] = i - stack.pollFirst();
+		}
+		stack.offerFirst(i);
+	}
+	return result;
+}
